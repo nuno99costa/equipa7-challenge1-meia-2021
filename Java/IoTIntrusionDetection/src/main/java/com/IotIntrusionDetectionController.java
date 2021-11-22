0fdc44bd03681a8c6d2589b200bb517bb79e31d6
Nuno Costa <nunopf99@gmail.com>
2021-11-22 02:39:33 +0000

    Adds CORS support

--- Java/IoTIntrusionDetection/src/main/java/com/IotIntrusionDetectionController.java
+++ Java/IoTIntrusionDetection/src/main/java/com/IotIntrusionDetectionController.java
@@ -4,10 +4,7 @@ import com.model.EnumVirus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.model.Connection;
 
@@ -25,6 +22,7 @@ public class IotIntrusionDetectionController {
         this.iotIntrusionDetectionService = iotIntrusionDetectionService;
     }
 
+    @CrossOrigin
     @RequestMapping(value = "/iotintrusiondetection",
             method = RequestMethod.GET, produces = "application/json")
     public Map<EnumVirus, Double> getVirusList(
